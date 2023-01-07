48126edce98a3d4570f4cc114d84775c27815773
devasmi <impokhrelasmita5@gmail.com>
2023-01-07 14:23:12 -0500

    added functionality to persist

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
 
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -34,18 +33,32 @@ $(function () {
     }
   }
 
-  $(".saveBtn").on("click", function (event) {
-    var timeBlockEl = $(event.target).parent("div")[0];
-    var textAreaEl = $(timeBlockEl).children().eq("1");
+  $(".saveBtn").on("click", function () {
+    var timeBlockEl = $(this).parent("div").eq(0);
+    var textAreaEl = $(timeBlockEl).children().eq(1);
     var timeBlockId = $(timeBlockEl).attr("id");
     var textAreaValue = $(textAreaEl).val();
     localStorage.setItem(timeBlockId, textAreaValue);
   });
+
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
+
+  // hour-12 div, textarea value = "hello world"
+  //var value = "hello";
+  for (var i = 0; i < localStorage.length; i++) {
+    var key = localStorage.key(i);
+    var value = localStorage.getItem(key);
+    $("#" + key) // #hour-10, #hour-12 etc
+      .children()
+      .eq("1")
+      .val(value);
+  }
+
   //
   // TODO: Add code to display the current date in the header of the page.
   $("#currentDay").text(dayjs().format("dddd, MMM D"));
 });
+//localStorage.getItem(timeBlockId);
